ec8eb79c3aafbe803c1dfc317ad9ba033a173b44
Lộc <locpham050100@gmail.com>
2023-04-14 13:43:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import Nav from "./views/Nav";
+
+import { useState } from "react";
+
+const App = () => {
+  let [name, setName] = useState("Caphe");
+
+  const [address, setAddress] = useState("");
+
+  const handleEventClick = (e) => {
+    setName(address);
+  };
+
+  const handleOnchangeInput = (e) => {
+    setAddress(e.target.value);
+  };
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lộc Lộc Cute thích {name} !</h1>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => handleOnchangeInput(e)}
+        />
+        <button type="button" onClick={(e) => handleEventClick(e)}>
+          Click me!
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
